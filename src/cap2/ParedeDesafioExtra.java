534601aa9fb2fb54eb3bcdbe2cb92dbab51b2cee
Marina Stanquini Santos <marinastq@gmail.com>
2024-06-25 20:19:02 -0300

    exemplos das paginas 32 a 38

---
+++ src/cap2/ParedeDesafioExtra.java
@@ -0,0 +1,13 @@
+package cap2;
+
+public class Parede {
+
+    public static void main(String[] args) {
+        double eco = 10.5;
+        double x;
+
+        x = eco / 2;
+
+        System.out.println(x);
+    }
+}
