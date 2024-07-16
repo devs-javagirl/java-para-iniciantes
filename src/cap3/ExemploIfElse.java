ebbe463c3f7eaec702678f7188166d17cb064fa0
Marina Stanquini Santos <marinastq@gmail.com>
2024-07-16 11:46:36 -0300

    Exemplos Cap3

--- src/cap3/ExemploIfElse.java
+++ src/cap3/ExemploIfElse.java
@@ -1,2 +1,15 @@
-package cap3;public class ExemploIfElse {
+package cap3;
+
+public class ExemploIfElse {
+    public static void main(String[] args) {
+        int i = 10, j = 1, k = 1;
+        int a, b = 2, c = 3, d = 4;
+
+        if(i == 10) {
+            if(j < 20) a = b;
+            if(k > 100) c = d;
+            else a = c; // esse else é referente a if(k > 100)
+        }
+        else a = d; // esse else é referente a if(i == 10)
+    }
 }
