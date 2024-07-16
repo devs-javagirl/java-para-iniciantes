ea5414a6384941ac7ca5959dc929b594030cb2b8
Marina Stanquini Santos <marinastq@gmail.com>
2024-07-16 11:46:21 -0300

    Exemplos Cap3

--- src/cap3/NoBreak.java
+++ src/cap3/NoBreak.java
@@ -1,2 +1,23 @@
-package cap3;public class NoBreak {
+package cap3;
+
+// Demonstra switch sem instruções break.
+class NoBreak {
+    public static void main(String args[]) {
+        int i;
+        for(i=0; i<=5; i++) {
+            switch(i) {
+                case 0:
+                    System.out.println("i is less than one");
+                case 1:
+                    System.out.println("i is less than two");
+                case 2:
+                    System.out.println("i is less than three");
+                case 3:
+                    System.out.println("i is less than four");
+                case 4:
+                    System.out.println("i is less than five");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/cap3/Switch1.java
+++ src/cap3/Switch1.java
@@ -1,2 +1,18 @@
-package cap3;public class Switch1 {
+package cap3;
+
+public class Switch1 {
+    public static void main(String[] args) {
+        int i = 2;
+        for(i=0; i<=5; i++) {
+            switch(i) {
+                case 1:
+                case 2:
+                case 3: System.out.println("i is 1, 2 or 3");
+                    break;
+                case 4: System.out.println("i is 4");
+                    break;
+            }
+            System.out.println();
+        }
+    }
 }
