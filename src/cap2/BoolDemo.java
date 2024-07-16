b502a13a691a28af158452f2225138d9f87de317
Marina Stanquini Santos <marinastq@gmail.com>
2024-07-16 11:34:16 -0300

    Exemplo Cap2 BoolDemo

--- src/cap2/BoolDemo.java
+++ src/cap2/BoolDemo.java
@@ -1,2 +1,25 @@
-package cap2;public class BoolDemo {
+package cap2;
+
+// Demonstra valores booleanos.
+class BoolDemo {
+    public static void main(String args[]) {
+        boolean b;
+
+        b = false;
+        System.out.println("b is " + b);
+
+        b = true;
+        System.out.println("b is " + b);
+
+        // um valor booleano pode controlar a instrução if
+        if(b)
+            System.out.println("This is executed.");
+
+        b = false;
+        if(b)
+            System.out.println("This is not executed.");
+
+        // o resultado de um operador relacional é um valor booleano
+        System.out.println("10 > 9 is " + (10 > 9));
+    }
 }
