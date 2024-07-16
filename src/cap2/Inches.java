23a2001254c388097526ce7e03175c02e344a54a
Marina Stanquini Santos <marinastq@gmail.com>
2024-07-16 11:38:20 -0300

    Exemplos Cap2

--- src/cap2/Inches.java
+++ src/cap2/Inches.java
@@ -1,2 +1,16 @@
-package cap2;public class Inches {
+package cap2;
+
+/*
+ Calcula quantas polegadas cúbicas há em
+ uma milha cúbica.
+*/
+class Inches {
+    public static void main(String args[]) {
+        long ci;
+        long im;
+        im = 5280 * 12;
+        ci = im * im * im;
+        System.out.println("There are " + ci +
+                " cubic inches in cubic mile.");
+    }
 }
--- src/cap2/PrePosFixado.java
+++ src/cap2/PrePosFixado.java
@@ -1,2 +1,10 @@
-package cap2;public class PrePosFixado {
+package cap2;
+
+public class PrePosFixado {
+    public static void main(String[] args) {
+        int i =0;
+        System.out.println(i + "   " + (++i));
+        System.out.println(i + "   " + (i++));
+        System.out.println(i);
+    }
 }
